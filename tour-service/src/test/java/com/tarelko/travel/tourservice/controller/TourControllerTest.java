42d2e0f6c17c06c399621f47685e19d16ea137e8
Vitali_Tarelko <PAutospace9997!>
2022-08-04 17:35:34 +0400

    Update tests

--- tour-service/src/test/java/com/tarelko/travel/tourservice/controller/TourControllerTest.java
+++ tour-service/src/test/java/com/tarelko/travel/tourservice/controller/TourControllerTest.java
@@ -1,7 +1,11 @@
 package com.tarelko.travel.tourservice.controller;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.Objects;
 import java.util.UUID;
 
+import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -12,6 +16,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -29,12 +34,21 @@ class TourControllerTest {
     @Test
     public void getTourById_success() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/api/v1/tours/{id}", UUID.randomUUID()))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(content().json(getResourceAsString("getByIdResponse.json")));
     }
 
     @Test
     public void getTours_success() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/api/v1/tours"))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(content().json(getResourceAsString("getAllResponse.json")));
+    }
+
+
+    private String getResourceAsString(String fileName) throws IOException {
+        return IOUtils.toString(
+                Objects.requireNonNull(this.getClass().getResourceAsStream(fileName)),
+                Charset.defaultCharset());
     }
 }
