bb6a9addba47e97147265f6556efe7eabbd68f23
Vitali <tareltos@gmail.com>
2022-08-04 17:50:03 +0400

    Feature/implement tour service app (#2)
    * Init project
    * Implement simple rest service
    * Update tests

---
+++ tour-service/src/test/java/com/tarelko/travel/tourservice/TourServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarelko.travel.tourservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
